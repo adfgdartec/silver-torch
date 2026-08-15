0165bf6a9fded826e44cad60579436aa3bf83fd6
Adit Rajaram <rajitadarth422@gmail.com>
2026-08-15 12:14:24 -0500

    Release 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.2.0 - 2026-08-15
+
+- Added a coordinated release version and reproducible OIDC-based PyPI publishing.
+
+## 0.1.0
+
+- Initial release of `silver-torch`.
--- src/silver_torch/__init__.py
+++ src/silver_torch/__init__.py
@@ -4,3 +4,5 @@ from .pipeline import SilverTorchPipeline, compile_silver, parse_silver
 from .spec import SilverPreprocessSpec
 
 __all__ = ["SilverPreprocessSpec", "SilverTorchPipeline", "compile_silver", "parse_silver"]
+
+__version__ = "0.2.0"
