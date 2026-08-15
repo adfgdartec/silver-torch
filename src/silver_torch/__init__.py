61388c01d373e7e1b3ac027a232bbe9cb83e0157
Adit Rajaram <rajitadarth422@gmail.com>
2026-08-15 13:24:39 -0500

    Add model and GPU execution foundations

--- src/silver_torch/__init__.py
+++ src/silver_torch/__init__.py
@@ -2,7 +2,9 @@
 
 from .pipeline import SilverTorchPipeline, compile_silver, parse_silver
 from .spec import SilverPreprocessSpec
+from .modeling import ModelBlueprint, TransformerConfig, build_model, build_transformer
 
-__all__ = ["SilverPreprocessSpec", "SilverTorchPipeline", "compile_silver", "parse_silver"]
+__all__ = ["SilverPreprocessSpec", "SilverTorchPipeline", "compile_silver", "parse_silver",
+           "ModelBlueprint", "TransformerConfig", "build_model", "build_transformer"]
 
 __version__ = "0.2.0"
